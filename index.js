1ee09ff903e2a0fcd06699883d4fd5895bf9a038
Evo <125100286+Evocryption@users.noreply.github.com>
2023-04-03 13:14:39 +0100

    Update index.js
    Added response number

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 function onSubmit(resolve) {
-    let discord_webhook = '' //Your discord webhook
-    let discord_embed = {
-        embeds: [{
-            type: 'rich', // do not change
-            title: 'New app!', ///Here would be the title
-            color: 00000, //Here would be the color the default is black
-            fields: [], //Leave this alone
-        }]
-    }
-    resolve.response.getItemResponses().forEach(function(comeback) {
-        let val = comeback.getResponse() || 'None'
-        discord_embed.embeds[0].fields.push({ name: comeback.getItem().getTitle(), value: val })
-    })
-    UrlFetchApp.fetch(discord_webhook, {
-        method: 'post',
-        payload: JSON.stringify(discord_embed),
-        contentType: 'application/json'
-    })
+  let discord_webhook = '' //Your discord webhook
+  let responses = resolve.source.getResponses();
+  let responseNumber = responses.length;
+  let discord_embed = {
+      embeds: [{
+          type: 'rich', // do not change
+          title: 'New Application! Response Number: #' + responseNumber,
+          color: 00000, //Here would be the color the default is black
+          fields: [], //Leave this alone
+      }]
+  }
+  resolve.response.getItemResponses().forEach(function(comeback) {
+      let val = comeback.getResponse() || 'None'
+      discord_embed.embeds[0].fields.push({ name: comeback.getItem().getTitle(), value: val })
+  })
+  UrlFetchApp.fetch(discord_webhook, {
+      method: 'post',
+      payload: JSON.stringify(discord_embed),
+      contentType: 'application/json'
+  })
 }
